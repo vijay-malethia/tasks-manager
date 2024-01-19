d52609b493cef0f1d6e0b5382d8ccc3162a048c8
Vijay Malethia <vijay.malethia@pizoneinfotech.com>
2024-01-19 11:52:01 +0530

    connet to server

---
+++ backend/config/connectDB.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose"); //Import mongoose
+
+//This function used to connect to database
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1); //exit connection if error occuures
+  }
+};
+module.exports = connectDB; //export DB connection
+
+/////Using this function in server.js file to connect to server and database/////////////////////////////
+
+// const connectDB = require("./config/connectDB"); //Import db connection file    (also import it)
+
+//Create Server
+// const createServer = async () => {
+//     try {
+//       app.listen(PORT, () => {
+//         console.log("Server Connected");
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   //Call connetDB and createServer
+//   const startServer = async () => {
+//     try {
+//       await connectDB();
+//       createServer();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   //Start Server
+//   startServer();
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
+const dotenv = require("dotenv").config(); //Using config we can initiate dotenv file
 const express = require("express"); //Import express
+const mongoose = require("mongoose"); //Import mongoose
 
-//Initial Express
+//Initiate Express
 const app = express();
 
-//Routes
+//--------------------------------------------------Routes-------------------------
+//Routes ("/",inital route)
 app.get("/", (req, res) => {
   res.send("Welcome to Home Page");
 });
 
+//About page
 app.get("/about", (req, res) => {
   res.send("Welcome to About Page");
 });
 
-//Change port on basis of production and development
-const PORT = process.env.PORT || 5000;
+//-------------------------------------------------Connection---------------------
 
-//Create server
-app.listen(PORT, () => {
-  console.log("hello world, server running on port &{PORT}");
-});
+const PORT = process.env.PORT || 5000; //Change port on basis of production and development
+
+//Using this you can connect db then start the server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {f
+      console.log("Server Connected");
+    });
+  })
+  .catch((error) => console.log(error));
