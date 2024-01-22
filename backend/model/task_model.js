e701f35fc5140400a81942f43b59a34841316297
Vijay Malethia <vijay.malethia@pizoneinfotech.com>
2024-01-22 10:47:01 +0530

    add schema

--- backend/model/task_model.js
+++ backend/model/task_model.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 
-
 //Create schema to structure our data
-
-const taskSchema = mongoose.Schema({
-    name:{
-        type: String,
-        requrired: [true,'Please enter task name'],
-
+const taskSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      requrired: [true, "Please enter task name"],
     },
-    completed:{
-        type:Boolean,
-        requrired:true
-    }
-});
+    completed: {
+      type: Boolean,
+      requrired: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
