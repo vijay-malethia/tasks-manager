1a5308687fb272ec844f2ce01fb394e92c212372
Vijay Malethia <vijay.malethia@pizoneinfotech.com>
2024-01-25 17:44:11 +0530

    fix

--- backend/controllers/task_controller.js
+++ backend/controllers/task_controller.js
@@ -20,7 +20,22 @@ const getAllTask = async (req, res) => {
   }
 };
 
+//Get a task with id
+const getTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json(`No task found with id:${id}`);
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 module.exports = {
   createTask,
   getAllTask,
+  getTaskById,
 };
--- backend/routes/task_routes.js
+++ backend/routes/task_routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createTask, getAllTask } = require("../controllers/task_controller");
+const {
+  createTask,
+  getAllTask,
+  getTaskById,
+} = require("../controllers/task_controller");
 
 //Router
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getAllTask);
+router.get("/api/getTaskById/:id", getTaskById);
 
 module.exports = router;
